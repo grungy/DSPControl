41629fb06611134d6525b5d79d9bae89e810ce41
Josh Marks <58739998+joshgov@users.noreply.github.com>
2022-12-12 15:52:59 -0800

    working send and receive code.

--- main.py
+++ main.py
@@ -1,36 +1,123 @@
 import sys
 import glob
 import serial
+import time
+from serial.tools.list_ports import comports
 
 
-def serial_ports():
-    """ Lists serial port names
+COM_PORT = "/dev/cu.usbserial-1420"
+BAUD_RATE = 19200
 
-        :raises EnvironmentError:
-            On unsupported or unknown platforms
-        :returns:
-            A list of the serial ports available on the system
+try:
+    raw_input
+except NameError:
+    # pylint: disable=redefined-builtin,invalid-name
+    raw_input = input   # in python3 it's "raw"
+    unichr = chr
+
+def ask_for_port():
+    """\
+    Show a list of ports and ask the user for a choice. To make selection
+    easier on systems with long device names, also allow the input of an
+    index.
     """
-    if sys.platform.startswith('win'):
-        ports = ['COM%s' % (i + 1) for i in range(256)]
-    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        # this excludes your current terminal "/dev/tty"
-        ports = glob.glob('/dev/tty[A-Za-z]*')
-    elif sys.platform.startswith('darwin'):
-        ports = glob.glob('/dev/tty.*')
-    else:
-        raise EnvironmentError('Unsupported platform')
-
-    result = []
-    for port in ports:
+    sys.stderr.write('\n--- Available ports:\n')
+    ports = []
+    for n, (port, desc, hwid) in enumerate(sorted(comports()), 1):
+        sys.stderr.write('--- {:2}: {:20} {!r}\n'.format(n, port, desc))
+        ports.append(port)
+    while True:
+        sys.stderr.write('--- Enter port index or full name: ')
+        port = raw_input('')
         try:
-            s = serial.Serial(port)
-            s.close()
-            result.append(port)
-        except (OSError, serial.SerialException):
+            index = int(port) - 1
+            if not 0 <= index < len(ports):
+                sys.stderr.write('--- Invalid index!\n')
+                continue
+        except ValueError:
             pass
-    return result
+        else:
+            port = ports[index]
+        return port
+
+def reader(ser):
+        """loop and copy serial->console"""
+        alive = True
+
+        try:
+            while alive:
+                # read all that is there or wait for one byte
+                data = ser.read(ser.in_waiting or 1)
+                if data:
+                    print(data)
+        except serial.SerialException:
+            alive = False
+            raise       # XXX handle instead of re-raise?
+
+def writer(self):
+        """\
+        Loop and copy console->serial until self.exit_character character is
+        found. When self.menu_character is found, interpret the next key
+        locally.
+        """
+        menu_active = False
+        try:
+            while self.alive:
+                try:
+                    c = self.console.getkey()
+                except KeyboardInterrupt:
+                    c = '\x03'
+                if not self.alive:
+                    break
+                if menu_active:
+                    self.handle_menu_key(c)
+                    menu_active = False
+                elif c == self.menu_character:
+                    menu_active = True      # next char will be for menu
+                elif c == self.exit_character:
+                    self.stop()             # exit app
+                    break
+                else:
+                    #~ if self.raw:
+                    text = c
+                    for transformation in self.tx_transformations:
+                        text = transformation.tx(text)
+                    self.serial.write(self.tx_encoder.encode(text))
+                    if self.echo:
+                        echo_text = c
+                        for transformation in self.tx_transformations:
+                            echo_text = transformation.echo(echo_text)
+                        self.console.write(echo_text)
+        except:
+            self.alive = False
+            raise
+
+
 
 
 if __name__ == '__main__':
-    print(serial_ports())
+    # COM_PORT = ask_for_port()
+
+    if COM_PORT:
+        # configure the serial connections (the parameters differs on the device you are connecting to)
+        ser = serial.Serial(
+                port=COM_PORT,
+                baudrate=BAUD_RATE,
+                parity=serial.PARITY_NONE,
+                stopbits=serial.STOPBITS_ONE,
+                bytesize=serial.EIGHTBITS,
+                rtscts=False
+            )
+
+        print(ser.is_open)
+        
+        ser.write(b"B")
+
+
+        ser.flush()
+
+        data = ser.read(1)
+
+        if data:
+            print(data)
+        ser.close()
